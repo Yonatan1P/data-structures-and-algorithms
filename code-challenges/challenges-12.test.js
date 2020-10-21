ece2284664271f0efc7d0697b1de520d7969fbfb
Yonatan Palagashvili <yonimotek@gmail.com>
2020-10-20 22:16:09 -0700

    finished 1 challenge

--- code-challenges/challenges-12.test.js
+++ code-challenges/challenges-12.test.js
@@ -46,9 +46,7 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  const sumArr = stores.map(callback(currentValue[index[array]])({
-    
-  }));
+  
 
 };
 

695390c239e0ac0a3ef8b04b2a27cf8d4c4ab91c
Yonatan Palagashvili <yonimotek@gmail.com>
2020-12-21 16:51:16 -0800

    fixed and added tests

--- python/challenges/repeated_word/repeated_word.py
+++ python/challenges/repeated_word/repeated_word.py
@@ -1,15 +1,20 @@
 from challenges.hashtable.hashtable import Hashtable
 
+
 def repeat_word(string):
     if not string:
         return False
     words = string.split(' ')
     table = Hashtable()
     for word in words:
-        word.lower()
-        if table.contains(word):
-            return word
-        table.add(word, word)
+
+        saved_word = ''
+        for char in word:
+            if char != '.' and '!' and '?' and '(' and ')' and ',' and ':' and ';':
+                saved_word += char
+        lower_case_word = saved_word.lower()
+
+        if table.contains(lower_case_word):
+            return saved_word
+        table.add(lower_case_word, lower_case_word)
     return False
-
-
--- python/tests/test_repeat_word.py
+++ python/tests/test_repeat_word.py
@@ -1,19 +1,29 @@
 from challenges.repeated_word.repeated_word import repeat_word
 
+
 def test_no_string():
-    string=''
+    string = ''
     actual = repeat_word(string)
     expected = False
     assert actual == expected
+
 
 def test_no_repeats():
-    string='Hi, my name is Yoni and I like toast!'
+    string = 'Hi, my name is Yoni and I like toast!'
     actual = repeat_word(string)
     expected = False
     assert actual == expected
 
+
 def test_one_repeats():
-    string='Hi, I am Yoni and I like toast!'
+    string = 'Hi, I am Yoni and I like toast!'
     actual = repeat_word(string)
     expected = 'I'
+    assert actual == expected
+
+
+def test_multiple_repeats():
+    string = 'Hi, my name is Yoni. And Yoni likes toast. And I am the person with the name Yoni, also I like toast!'
+    actual = repeat_word(string)
+    expected = 'Yoni'
     assert actual == expected
